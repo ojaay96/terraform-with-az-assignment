1a373c01bc1b8e9bfd65612a0a51ce6a25e00bfe
James John <106777825+Ojaay96@users.noreply.github.com>
2025-11-04 00:40:52 +0100

    added backend.tf

--- virtual_network/backend.tf
+++ virtual_network/backend.tf
@@ -10,8 +10,8 @@
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-terraform-state"
-    storage_account_name = "tfstateStorageacct"
+    resource_group_name  = "rg-jaay-terraform"
+    storage_account_name = "jaaytfstorageacct"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
